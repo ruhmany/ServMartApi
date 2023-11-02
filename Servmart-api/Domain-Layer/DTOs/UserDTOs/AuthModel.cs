6a4487b13cce7388592187835af2f6b242a16bf8
abdelrahmman97 <mabdelrahmman22@gmail.com>
2023-11-02 19:30:27 +0200

    added username, profilePic, email to login & register return data

--- Servmart-api/Domain-Layer/DTOs/UserDTOs/AuthModel.cs
+++ Servmart-api/Domain-Layer/DTOs/UserDTOs/AuthModel.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace Domain_Layer.DTOs.UserDTOs
 {
-    public class AuthModel
-    {
-        public string Message { get; set; }
-        public bool IsAuthenticated { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public List<string> Role { get; set; }
-        public string Token { get; set; }
-        public DateTime ExpiresOn { get; set; }
-    }
+	public class AuthModel
+	{
+		public string Message { get; set; }
+		public bool IsAuthenticated { get; set; }
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public List<string> Role { get; set; }
+		public string Token { get; set; }
+		public DateTime ExpiresOn { get; set; }
+		public string ProfilePic { get; set; }
+	}
 }
